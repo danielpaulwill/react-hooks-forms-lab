5081ffcac88f5183bd7d56268dc60d10369fac1e
Daniel Williams <danielpaulwill@gmail.com>
2022-01-04 14:19:40 -0500

    Done.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange, search }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" placeholder="Search..." value={search} onChange={onSearchChange} />
+
+{/* 
+App
+  Header
+  ShoppingList
+    ItemForm
+    Filter
+    Item */}
+
+
+      {/* 
+      Finally, after making those changes, you'll need to use that state value 
+      to determine which items are being displayed on the page, similar to how 
+      the category dropdown works. */}
+
+
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,34 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchBar, setSearchBar] = useState("");
+
+console.log(items.includes(searchBar))
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  function handleSearchChange(e) {
+    setSearchBar(e.target.value)
+  }
+  
+  // const searchItemsToDisplay = items.filter((item) => {
+  //   item.includes(searchBar)
+  //   return item
+  // })
+  // console.log(searchItemsToDisplay)
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
-
+    
     return item.category === selectedCategory;
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} search={searchBar} onSearchChange={handleSearchChange}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
